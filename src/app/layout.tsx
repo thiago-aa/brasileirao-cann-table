17bdf64afd1febaefd3d7d27d2d392a0073be90b
Thiago <almeida.thiagoalvesde@gmail.com>
2026-07-28 22:33:04 -0300

    Add Google Analytics scripts to RootLayout for tracking user interactions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, Chakra_Petch } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-primary",
@@ -28,7 +30,21 @@ export default function RootLayout({
       lang="en"
       className={`${spaceGrotesk.variable} ${chakraPetch.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <body className="min-h-full flex flex-col">
+      {children}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-8737Z3D89Y`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8737Z3D89Y');
+        `}
+      </Script>  
+    </body>
     </html>
   );
 }
