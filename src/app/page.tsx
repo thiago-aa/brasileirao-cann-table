4fd298fdf94b35a58dddf73c213fbcbc23fbaece
Thiago <almeida.thiagoalvesde@gmail.com>
2026-09-17 21:57:45 -0300

    Update champion colors to HSL values and enhance information link in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,9 @@ export default async function Home() {
             <div className="flex flex-col items-center gap-2 relative w-1/1 md:w-3/5">
               <div className="text-center flex-1">
                 <h1 className="md:text-3xl text-center font-primary">CLASSIFICAÇÃO CANN - BRASILEIRÃO</h1>
-                <p className="md:text-2xl text-intermed-dark font-primary">(EM ESCALA DE PONTOS)</p>
+                <p className="md:text-2xl text-intermed-dark font-primary">
+                  (EM ESCALA DE PONTOS, <a className='underline text-link' href="https://en.wikipedia.org/wiki/Cann_table" target="_blank">SAIBA MAIS</a>)
+                </p>
               </div>
               <DownloadButton/>
             </div>
